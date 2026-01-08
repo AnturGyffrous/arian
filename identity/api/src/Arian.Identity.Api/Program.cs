faa28d05945eef72f6af98940424822470242e97
Owain Williams <owain@hedfan.uk>
2026-01-08 15:57:57 +0000

    Use CORS and allow origin if the URI is loopback

--- identity/api/src/Arian.Identity.Api/Program.cs
+++ identity/api/src/Arian.Identity.Api/Program.cs
@@ -1,7 +1,15 @@
 using Arian.Identity.Api;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy => policy.SetIsOriginAllowed(origin => new Uri(origin).IsLoopback));
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 var clientDictionary = new Dictionary<string, Client>
 {
